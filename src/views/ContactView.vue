2e6dde4d4f2411e6f9305e9f0f7bb712a7ece535
tang1811 <satang1811@gmail.com>
2026-01-11 16:40:02 +0700

    update img in ContactView

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -1,5 +1,7 @@
 <template lang="">
     <div class="contact">
-      <h1>This is an contact page</h1>
+      <h1>This is a contact page</h1>
+        <h3>Tel. 065-959-7973</h3>
+      <img alt="Vue logo" src="../assets/Myimg.jpg">
     </div>
   </template>
